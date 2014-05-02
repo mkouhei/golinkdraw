828a5cbc9cb25131b9e5e27ae93a85669ded730a
Kouhei Maeda <mkouhei@palmtb.net>
2014-05-02 09:33:08 +0900

    separate view template from main.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- main.go
+++ main.go
@@ -3,37 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"text/template"
 )
-
 
 var (
 	s = &http.Server{
 		Addr: ":8080",
 	}
-	homeTempl = template.Must(template.ParseFiles("templates/index.html"))
 )
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	var v = struct {
-		Host string
-		Data string
-	}{
-		r.Host,
-		StringSVG(),
-	}
-	homeTempl.Execute(w, &v)
-}
 
 func main() {
 	http.Handle("/", http.HandlerFunc(serveHome))
---
+++ view_template.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+var (
+	homeTempl = template.Must(template.ParseFiles("templates/index.html"))
+)
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	var v = struct {
+		Host string
+		Data string
+	}{
+		r.Host,
+		StringSVG(),
+	}
+	homeTempl.Execute(w, &v)
+}
