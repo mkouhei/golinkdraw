a38ed92e1f747c68beee7edebb711dd2a4e0b43e
Kouhei Maeda <mkouhei@palmtb.net>
2014-06-11 07:35:47 +0900

    Removed comment out code.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- modules/network.go
+++ modules/network.go
@@ -14,10 +14,7 @@ func (canv Canvas) Network() *svg.SVG {
 	canvas := svg.New(canv.W)
 	canvas.Start(canv.Width, canv.Height)
 
-	//x := canv.Width / 20
-	//y := canv.Height / 20
 	d := 5 // diameter
-	//span := 50
 	linestyle := "fill:none;stroke:black"
 	circlestyle := "fill:gray"
 
@@ -29,17 +26,6 @@ func (canv Canvas) Network() *svg.SVG {
 		ypoly[i] = rand.Intn(canv.Height)
 		canvas.Circle(xpoly[i], ypoly[i], d, circlestyle)
 	}
-	/*
-		xpoly[0] = x
-		ypoly[0] = y
-		xpoly[1] = x + span*2
-		ypoly[1] = y + span*3
-		xpoly[2] = x + span*4
-		ypoly[2] = y + span*5
-		canvas.Circle(xpoly[0], ypoly[0], d, circlestyle)
-		canvas.Circle(xpoly[1], ypoly[1], d, circlestyle)
-		canvas.Circle(xpoly[2], ypoly[2], d, circlestyle)
-	*/
 	canvas.Polyline(xpoly, ypoly, linestyle)
 
 	canvas.End()
