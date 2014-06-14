8462c6326166a217a9379b416bd9a0c8c1228712
Kouhei Maeda <mkouhei@palmtb.net>
2014-06-14 23:04:07 +0900

    Added description.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- modules/network.go
+++ modules/network.go
@@ -8,6 +8,7 @@ package modules
 import (
 	"github.com/ajstarks/svgo"
 	"math/rand"
+	"strconv"
 )
 
 func (canv Canvas) Network() *svg.SVG {
@@ -16,15 +17,17 @@ func (canv Canvas) Network() *svg.SVG {
 
 	d := 5 // diameter
 	linestyle := "fill:none;stroke:black"
+	textstyle := "font-size:10px"
 	circlestyle := "fill:gray"
 
-	nd := 511
+	nd := 100
 	xpoly := make([]int, nd)
 	ypoly := make([]int, nd)
 	for i := 0; i < nd; i++ {
 		xpoly[i] = rand.Intn(canv.Width)
 		ypoly[i] = rand.Intn(canv.Height)
 		canvas.Circle(xpoly[i], ypoly[i], d, circlestyle)
+		canvas.Text(xpoly[i]+d*2, ypoly[i]-d*2, "("+strconv.Itoa(xpoly[i])+","+strconv.Itoa(ypoly[i])+")", textstyle)
 		if i > 0 {
 			canvas.Line(xpoly[i-1], ypoly[i-1], xpoly[i], ypoly[i], linestyle)
 		}
